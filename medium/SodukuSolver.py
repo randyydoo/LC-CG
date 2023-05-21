a51a842a85fe93a92e61ab8f530be8559c1f1209
Randy Do <randyydoo@csu.fullerton.edu>
2023-05-20 23:06:34 -0700

    done

--- medium/SodukuSolver.py
+++ medium/SodukuSolver.py
@@ -1,29 +1,21 @@
-def solve(board):
-    dic = {"row": [[] for i in range(9)], "col" : [[] for i in range(9)], 'board': [[] for i in range(9)]}
-    boardNum = 0
-    row, col = 0, 0
-    for i in range(len(board)):
-        for j in range(len(board)):
-            num = board[i][j]
-            #figure out algo for updating board number using row and col
-            if num in dic['row'][i] or num in dic['col'][j] or num in dic['board'][boardNum]:
-                return False
-            else:
-                dic['row'][i].append(num)
-                dic['col'][j].append(num)
-                dic['board'][boardNum].append(num)
-            col += 1
-        row += 1
-    return True
-
-
-b=[["8","3",".",".","7",".",".",".","."]
-,["6",".",".","1","9","5",".",".","."]
-,[".","9","8",".",".",".",".","6","."]
-,["20",".",".",".","6",".",".",".","3"]
-,["4",".",".","8",".","3",".",".","1"]
-,["7",".",".",".","2",".",".",".","6"]
-,[".","6",".",".",".",".","2","8","."]
-,[".",".",".","4","1","9",".",".","5"]
-,[".",".",".",".","8",".",".","7","9"]]
-print(solve(b))
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        dic = {"row": [[] for i in range(9)], "col" : [[]  for i in range(9)], 'board': [[] for i in range(9)]}
+        curr = 0
+        for i in range(len(board)):
+            temp = 0
+            if i != 0 and i % 3 == 0:
+                curr += 3
+            for j in range(len(board)):
+                num = board[i][j]
+                if j != 0 and j % 3 == 0:
+                    temp += 1
+                if num == ".":
+                    continue
+                if num in dic['row'][i] or num in dic['col'][j] or num in dic['board'][curr + temp]:
+                    return False
+                else:
+                    dic['col'][j].append(num)
+                    dic['row'][i].append(num)
+                    dic['board'][curr + temp].append(num)
+        return True
