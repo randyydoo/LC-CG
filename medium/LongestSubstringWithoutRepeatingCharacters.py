1b21234ada81536fa96842da0e97638760533b44
Randy Do <randyydoo@csu.fullerton.edu>
2023-05-30 23:25:53 -0700

    new sol

--- medium/LongestSubstringWithoutRepeatingCharacters.py
+++ medium/LongestSubstringWithoutRepeatingCharacters.py
@@ -1,20 +1,14 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) == 0:
-            return 0
-        max = 1
-        i = 0
-        while i <= len(s) - 1:
+        curr = 0
+        for i in range(len(s)):
             temp = set()
             temp.add(s[i])
-            j = i + 1
-            while True: 
-                if j > len(s) - 1 or s[j] in temp:
+            for j in range(i + 1, len(s)):
+                if s[j] in temp:
                     break
                 else:
                     temp.add(s[j])
-                    j += 1
-            if len(temp) > max:
-                max = len(temp)
-            i += 1
-        return max
+            if len(temp) > curr:
+                curr = len(temp)
+        return curr
