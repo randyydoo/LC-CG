1f1ba8fdfde62e06494cd3835f7c7dd1d62e16d6
Randy Do <randyydoo@csu.fullerton.edu>
2023-06-10 14:56:52 -0700

    edited 'mergetwosortedlists'

--- easy/MergeTwoSortedLists.py
+++ easy/MergeTwoSortedLists.py
@@ -8,16 +8,10 @@ class Solution:
         head = ListNode()
         curr = head
         while list1 and list2:
-            if list1.val < list2.val:
+            if list1.val <= list2.val:
                 curr.next = list1
                 list1 = list1.next
             elif list1.val > list2.val: 
-                curr.next = list2
-                list2 = list2.next
-            else:
-                curr.next = list1
-                list1 = list1.next
-                curr = curr.next
                 curr.next = list2
                 list2 = list2.next
             curr = curr.next
