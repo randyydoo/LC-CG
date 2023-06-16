a9ef2c5a32bc327d62c22a4fa373be96f1b7fbb8
Randy Do <randyydoo@csu.fullerton.edu>
2023-06-15 22:11:56 -0700

    fixed solution for containter-with-most-water

--- medium/ContainerMostWater.py
+++ medium/ContainerMostWater.py
@@ -1,17 +1,16 @@
-#two pointer
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        res = 0
+        ans = 0
         left = 0
         right = len(height) - 1
         while left <= right:
-            max_height = min(height[left],height[right])
-            distance = index(right) - index(left)
-            area = max_height * distance
-            if area > res:
-                res = area
+            length = min(height[left],height[right])
+            width = right - left
+            area = length * width
+            if area > ans:
+                ans = area
             if height[left] > height[right]:
                 right -= 1
             else:
                 left += 1
-        return re
+        return ans
