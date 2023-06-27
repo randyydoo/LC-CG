09a5fcaa6ad82ba7851a6106fb4a892fddce1937
Randy Do <randyydoo@csu.fullerton.edu>
2023-06-27 15:10:22 -0700

    finished add two numbers

---
+++ medium/AddTwoNumbers.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        ans = dummy
+        curr1, curr2 = l1, l2
+        carry = 0
+        while curr1 and curr2:
+            s = curr1.val + curr2.val + carry
+            if s > 9:
+                carry = 1
+                ans.next = ListNode(s % 10)
+            else:
+                carry = 0
+                ans.next = ListNode(s)
+            ans, curr1, curr2 = ans.next, curr1.next, curr2.next
+        while curr1:
+            s = curr1.val + carry
+            if s > 9:
+                carry = 1
+                ans.next = ListNode(s % 10)
+            else:
+                carry = 0
+                ans.next = ListNode(s)
+            ans, curr1 = ans.next, curr1.next
+        while curr2:
+            s = curr2.val + carry
+            if s > 9:
+                carry = 1
+                ans.next = ListNode(s % 10)
+            else:
+                carry = 0
+                ans.next = ListNode(s)
+            ans, curr2 = ans.next, curr2.next
+        if carry == 1:
+            ans.next = ListNode(1)
+        return dummy.next
