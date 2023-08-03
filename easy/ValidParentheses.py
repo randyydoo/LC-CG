24d3b719c2ddbddd86565c2e628e6d9e8fc2614e
Randy Do <randyydoo@csu.fullerton.edu>
2023-08-04 07:33:46 +0900

    cleaned up solution for valid parentheses

--- easy/ValidParentheses.py
+++ easy/ValidParentheses.py
@@ -1,17 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        close = {'}', ')', ']'}
-        for i in s:
-            stack.append(i)
-            if i in close:
-                if i is ']' and stack[stack.index(i) - 1] is '[':
-                    stack.pop()
-                    stack.pop()
-                elif i is '}' and stack[stack.index(i) - 1] is '{':
-                    stack.pop()
-                    stack.pop()
-                elif i is ')' and stack[stack.index(i) - 1] is '(':
-                    stack.pop()
-                    stack.pop()
-        return len(stack) is 0
+        for p in s:
+            if stack and p == ']' and stack[-1] == '[':
+                stack.pop()
+            elif stack and p == '}' and stack[-1] == '{':
+                stack.pop()
+            elif stack and p == ')' and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(p)
+
+        return len(stack) == 0
