086e480d72ffe118dfeb8372fe91b4d499750079
Randy Do <randyydoo@gmail.com>
2023-09-07 16:30:13 -0700

    removed conditions for max depth binary tree

--- easy/MaxDepthBinaryTree.py
+++ easy/MaxDepthBinaryTree.py
@@ -6,11 +6,7 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-
         def dfs(node):
-
             if not node:
                 return 0
 
