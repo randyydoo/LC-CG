eb87471b00ce24f01abfbad479119716181c2f22
Randy Do <randyydoo@gmail.com>
2023-09-23 14:17:42 -0700

    added sol for kth smallest element in bst

---
+++ medium/KthSmallestBST.py
@@ -0,0 +1,41 @@
+# Solution 1
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        ans = []
+        def dfs(node):
+            nonlocal ans
+            if not node:
+                return
+            ans.append(node.val)
+            return dfs(node.left) or dfs(node.right)
+
+        dfs(root)
+        ans = sorted(ans)
+        return ans[k-1]
+
+# Soluition 2
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        curr = root
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            k -= 1
+            if k == 0:
+                return curr.val
+            curr = curr.right
