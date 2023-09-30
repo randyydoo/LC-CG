d48a5d1d22ec56895eca2fc6f4ee82544919797c
Randy Do <randyydoo@gmail.com>
2023-09-29 17:33:58 -0700

    added solution for binary tree paths

---
+++ easy/BinaryTreePaths.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        ans = []
+        def dfs(root, s):
+            nonlocal ans
+            if root and not root.left and not root.right:
+                s += f"{root.val}"
+                ans.append(s)
+                return
+
+            elif not root:
+                return 
+
+            s += f"{root.val}->"
+            dfs(root.left,s)
+            dfs(root.right,s)
+                
+            return
+
+        dfs(root, '')
+        return ans
