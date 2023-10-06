6e3cc53c1a48c7887157127c0d1d3ba4c840c466
Randy Do <randyydoo@gmail.com>
2023-10-05 17:22:20 -0700

    added solution for symetric tree

---
+++ medium/SymetricTree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def dfs(l, r):
+            if not l and not r:
+                return True
+            
+            elif (not l and r) or (l and not r) or (l.val != r.val):
+                return False
+            
+            return dfs(l.left,r.right) and dfs(l.right, r.left)
+
+        return dfs(root.left, root.right)
