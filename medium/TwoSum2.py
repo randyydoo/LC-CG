12e11de9f26ce521aa491c4ca255d65682e5a799
Randy Do <89406937+randyydoo@users.noreply.github.com>
2023-10-22 14:54:31 -0700

    updated solution Two Sum 2.

--- medium/TwoSum2.py
+++ medium/TwoSum2.py
@@ -1,13 +1,11 @@
-#two pointer
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        left = 0
-        right = len(numbers) - 1
-        while left <= right:
-            if numbers[left] + numbers[right] == target:
-                return [left + 1, right + 1]
-            elif numbers[left] + numbers[right] > target:
-                right -= 1
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:   
+        l, r = 0, len(numbers)-1
+        while 1:
+            s = numbers[l] + numbers[r]
+            if s == target:
+                return [l+1,r+1]
+            elif s > target:
+                r -= 1
             else:
-                left += 1
-
+                l += 1
