4807f7020ff5ec294942471dd9c552aad10f186c
randyydoo <randyydoo@gmail.com>
2023-11-20 20:34:23 -0800

    added Count Nice Pairs in an Array

--- easy/ContainsDuplicate.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        temp = set()
-        for i in nums:
-            if i in temp:
-                return True
-            temp.add(i)
-        return False
--- easy/TwoSum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hMap = {}
-        for i in range (0, len(nums)):
-            key = target - nums[i]
-            if key not in hMap:
-                hMap[nums[i]] = i
-            elif key in hMap:
-                return hMap[key],i 
