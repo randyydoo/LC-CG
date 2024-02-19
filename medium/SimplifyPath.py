092d008262d1d491fa89eb7bd78816600f312065
randyydoo <randyydoo@gmail.com>
2024-02-19 14:11:06 -0800

    added Simplify Path

---
+++ medium/SimplifyPath.py
@@ -0,0 +1,12 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        path = path.split("/")
+        stack = []
+        for c in path:
+            if c == "..":
+                if stack:
+                    stack.pop()
+            elif c != "" and c != ".":
+                stack.append(c)
+                
+        return '/'+"/".join(stack)
